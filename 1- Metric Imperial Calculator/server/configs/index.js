e05eda3a90fdea11526814bd06592ecf3b14a951
mehmood-ahmad <ahmad.khan@keeptruckin.com>
2019-04-21 20:00:34 +0500

    🚧 WIP: add cors, xssFilter and configs

---
+++ 1- Metric Imperial Calculator/server/configs/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    sitename: 'Metric Imperial Converter - Dev',
+    origin: 'http://localhost:',
+  },
+
+  production: {
+    sitename: 'Metric Imperial Converter',
+    origin: 'http://localhost:',
+  },
+};
--- 1- Metric Imperial Calculator/server/index.js
+++ 1- Metric Imperial Calculator/server/index.js
@@ -3,18 +3,26 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
+const cors = require('cors');
 
 // Internal Imports
 const routes = require('./routes');
+const config = require('./configs');
 
 // Initialisatons.
 const app = express();
 
 // App Settings
+app.use(cors({
+  origin: config[process.env.NODE_ENV].origin + process.env.PORT,
+}));
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
 app.use(helmet.noSniff());
+app.use(helmet.xssFilter());
+// Set Locals
+app.locals.title = config[app.get('env')].sitename;
 
 // Routes.
 app.use('/', routes);
